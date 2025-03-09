b513e04a82010a27b6b5d038ebde8f29c693e5c2
Junaid-Khan-46 <junaidameerkhan46@gmail.com>
2025-03-09 11:44:49 +0500

    add images and import tkinter

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter
+from tkinter import *
+
+root = Tk()
+root.title("My First GUI")
+root.geometry("400x650+400+100")  # Corrected geometry string
+root.resizable(False, False)
+root.mainloop()
